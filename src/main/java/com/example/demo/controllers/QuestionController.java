e42f71095ea54d15e011d0b66b0eb355c121c37c
Tara Johnson <tara@madface.com>
2018-08-05 23:49:46 -0700

    Updated route to /polls from /questions.

--- src/main/java/com/example/demo/controllers/QuestionController.java
+++ src/main/java/com/example/demo/controllers/QuestionController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping ("/questions")
+@RequestMapping ("/polls")
 public class QuestionController {
     @Autowired
     QuestionRepository questionRepository;
