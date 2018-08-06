69d167d41382b953eaba7010de984e7ba1eaee7d
Tara Johnson <tara@madface.com>
2018-08-05 23:24:50 -0700

    Upvote order is sorting.

--- src/main/java/com/example/demo/controllers/QuestionController.java
+++ src/main/java/com/example/demo/controllers/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +20,7 @@ public class QuestionController {
     @ResponseBody
     public List<Question> getAll() {
         List<Question> question = questionRepository.findAll();
+        Collections.sort(question);
         return question;
     }
 
--- src/main/java/com/example/demo/model/Question.java
+++ src/main/java/com/example/demo/model/Question.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="questionpoll")
-public class Question {
+public class Question implements Comparable<Question>{
     @Id
     @GeneratedValue(generator = "question_generator")
     @SequenceGenerator(
@@ -21,5 +21,16 @@ public class Question {
 
     public Question(String question){
         this.question = question;
+    }
+
+    @Override
+    // return -1 if this is less than the other one
+    // return 0 if these two things are equal
+    // return 1 if this is greater than the other one
+    public int compareTo(Question o) {
+        System.out.println(this.question + " had " + this.votes);
+        System.out.println(o.question + " had " + o.votes);
+        System.out.println("result: " + (this.votes - o.votes));
+        return o.votes - this.votes;
     }
 }
